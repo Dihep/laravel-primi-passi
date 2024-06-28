7d05c7a7f6746974f51ca608b48653b14a793013
Dihep <dihep.demayo@gmail.com>
2024-06-28 10:00:31 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel test</title>
+    <link rel="stylesheet" href=".../public/style.css">
+</head>
+
+<body>
+    <h1>Questa è una prova per verificare che Laravel funzioni.</h1>
+</body>
+</html>
